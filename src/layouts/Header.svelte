741b4efbea28e9f6bed4baa644db4c203ebd785c
ofnullable <ofnullable@gmail.com>
2020-06-26 02:44:20 +0900

    :construction: Add toggle header menu event, add default props to button

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -1,11 +1,30 @@
-<nav class="flex items-center justify-between flex-wrap bg-green-500 p-4">
+<script>
+  import { css } from 'emotion';
+
+  let openMenu = false;
+
+  $: menuStyle = openMenu
+    ? css`
+        display: block;
+      `
+    : css`
+        display: none;
+      `;
+
+  const toggleMenu = () => {
+    openMenu = !openMenu;
+  };
+</script>
+
+<nav class="flex items-center justify-between flex-wrap bg-green-500 p-4 relative">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <a href="/" class="font-semibold text-xl tracking-tight">sapper</a>
   </div>
   <div class="block lg:hidden">
     <button
-      class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white
+      class="flex items-center px-3 py-2 border rounded text-green-200 border-green-400 hover:text-white
       hover:border-white"
+      on:click={toggleMenu}
     >
       <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <title>Menu</title>
@@ -13,19 +32,19 @@
       </svg>
     </button>
   </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+  <div class={['w-full bg-green-500  lg:block flex-grow lg:flex lg:items-center lg:w-auto', menuStyle].join(' ')}>
     <div class="text-sm lg:flex-grow">
       <a href="/users">
-        <span class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Users</span>
+        <span class="block mt-4 lg:inline-block lg:mt-0 text-green-200 hover:text-white mr-4">Users</span>
       </a>
-      <span class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Examples</span>
-      <span class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">Blog</span>
+      <span class="block mt-4 lg:inline-block lg:mt-0 text-green-200 hover:text-white mr-4">Examples</span>
+      <span class="block mt-4 lg:inline-block lg:mt-0 text-green-200 hover:text-white">Blog</span>
     </div>
     <div>
       <a
         href="/login"
         class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
-        hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0"
       >
         Login
       </a>
@@ -34,7 +53,7 @@
         rel="noopener noreferrer"
         href="https://github.com/ofnullable/sapper-starter"
         class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
-        hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0"
       >
         Github
       </a>
